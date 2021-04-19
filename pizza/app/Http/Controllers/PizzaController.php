4db4044eaf9ac7cb67ce1090c49dfa28b226ee23
razikusman <razikusman@gmail.com>
2021-04-19 23:02:28 +0530

    adding __construct on controll class wil make all routes protected

--- pizza/app/Http/Controllers/PizzaController.php
+++ pizza/app/Http/Controllers/PizzaController.php
@@ -7,6 +7,10 @@ use App\Models\Pizza;
 
 class PizzaController extends Controller
 {
+
+    public function __construct(){
+        $this -> middleware('auth');
+    }
     // show all
     public function index(){
 
