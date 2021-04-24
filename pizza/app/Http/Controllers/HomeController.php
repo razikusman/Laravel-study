3525c92f738bb2c2d79a08433e62f195e208b889
razikusman <razikusman@gmail.com>
2021-04-25 02:01:59 +0530

    using less code for showing mesege by using with tag

--- pizza/app/Http/Controllers/HomeController.php
+++ pizza/app/Http/Controllers/HomeController.php
@@ -42,12 +42,10 @@ class HomeController extends Controller
 
         if($request->hasfile('image')){
             User::uploadImage($request->image);
-            $request->session()->flash('msg' , 'User Image Uploaded.');
-            return redirect()->back();
+            return redirect()->back()->with('msg' , 'User Image Uploaded.');
         }
 
-        $request->session()->flash('err' , 'User Image Upload fail.');
-        return redirect()->back();
+        return redirect()->back()->with('err' , 'User Image Upload fail.');
     }
 
     
